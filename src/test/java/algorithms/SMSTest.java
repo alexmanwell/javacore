610c0077af4533ae749da1b615c7952430f94f1b
alexmanwell <alexandr.manvelyan@yandex.ru>
2022-04-26 02:03:49 +0300

    rollback from junit 5 to junit4

--- src/test/java/algorithms/SMSTest.java
+++ src/test/java/algorithms/SMSTest.java
@@ -1,7 +1,7 @@
 package algorithms;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class SMSTest {
 
